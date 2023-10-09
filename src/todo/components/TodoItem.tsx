63e86502cde259a45f0d7e9dc7a05ef16878124e
CESCO <cescovalledev@gmail.com>
2023-10-09 17:43:04 -0600

    toggleTodo with reducer

--- src/todo/components/TodoItem.tsx
+++ src/todo/components/TodoItem.tsx
@@ -1,13 +1,28 @@
+import { useContext } from "react";
 import { Todo } from "../interfaces/interfaces";
+import { TodoContext } from "../context/TodoContexts";
 
 interface props {
   todo: Todo;
 }
 
 export const TodoItem = ({ todo }: props) => {
+  const { toggleTodo } = useContext(TodoContext);
+
   const handleDbClick = () => {
-    console.log("handleDbClick: ", todo.desc);
+    // console.log("handleDbClick: ", todo.desc);
+    toggleTodo(todo.id);
   };
 
-  return <li onDoubleClick={handleDbClick}>{todo.desc}</li>;
+  return (
+    <li
+      style={{
+        cursor: "pointer",
+        textDecoration: todo.completed ? "line-through" : "none",
+      }}
+      onDoubleClick={handleDbClick}
+    >
+      {todo.desc}
+    </li>
+  );
 };
